5da0fb8f9f9fb41d1f53ae3b97e37fd3b1bcd9ca
Zilong <91376809+Xiezilonger@users.noreply.github.com>
2023-08-02 15:58:20 +0800

    Add files via upload

---
+++ user-center-backend/src/main/java/com/yupi/usercenter/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.yupi.usercenter.mapper;
+
+import com.yupi.usercenter.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 用户 Mapper
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+// [编程知识星球](https://yupi.icu) 零基础快速入门编程，不走弯路！30+ 原创学习路线和专栏、1000+ 编程精华文章、500+ 编程学习指南、20T+ 编程资源汇总
+
